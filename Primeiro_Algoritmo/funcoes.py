caf58b6acf1f9d5651ed2c43a9cdc3158fbe550c
Gabriel <gabrielconceicao569@gmail.com>
2024-01-19 18:55:08 -0300

    Funções

---
+++ Primeiro_Algoritmo/funcoes.py
@@ -0,0 +1,21 @@
+# Funções
+
+# def: encapsulamento do código
+# 1- Sair de casa
+# 2- Atravessar a rua
+# 3- Entrar no mercado
+
+def numerosAleatorios():
+    print(1)
+    print(2)
+    print(18)
+    print("Dentro da função")
+
+def somarValores(numero1, numero2):
+    soma = numero1 + numero2
+    print(soma)
+    return soma
+
+resultado = somarValores(20, 30)
+resultado = resultado + 10
+print(resultado)
