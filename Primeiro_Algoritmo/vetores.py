c69936f4e62a11e2cdcabe10073187a7dc241af8
Gabriel <gabrielconceicao569@gmail.com>
2024-01-19 18:54:45 -0300

    Vetores

---
+++ Primeiro_Algoritmo/vetores.py
@@ -0,0 +1,18 @@
+# VETORES -> Os vetores podem ser guardados em vários dados/Sistemas de índices
+
+#meuVetor = [4,30,12,23,7,14]
+#meuVetor[4] = 19
+#print(meuVetor[0])
+
+#precosProdutos = [3,6,9]
+#print(precosProdutos[0])
+
+meuVetor = [1,2,3,4,5,6,7,8,9,10]
+
+for i in range(0,10):
+    meuVetor[i] = meuVetor[i] * 2
+
+print(meuVetor)
+
+for i in meuVetor:
+    print(i)
